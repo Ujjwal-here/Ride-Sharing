749404398e9d31790d060982a8e9c098b759da88
Ujjwal <ujjwalsbx@gmail.com>
2025-09-17 00:45:20 +0530

    move: ridefare model into its own file

--- services/trip-service/internal/domain/trip.go
+++ services/trip-service/internal/domain/trip.go
@@ -2,18 +2,9 @@ package domain
 
 import (
 	"context"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type RideFareModel struct {
-	ID                primitive.ObjectID
-	UserID            string
-	PackageSlug       string
-	TotalPriceInCents float64
-	ExpiresAt         time.Time
-}
 
 type TripModel struct {
 	ID       primitive.ObjectID
