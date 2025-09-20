ada9c99c80a3a1d03d7919b7361940b20c34b5e6
Ujjwal <ujjwalsbx@gmail.com>
2025-09-20 16:07:47 +0530

    feat(api-gateway/ws.go): add WebSocket handlers for drivers and riders
    - Add handleDriversWebSocket handler for /ws/drivers endpoint
    - Add handleRidersWebSocket handler for /ws/riders endpoint
    - Configure WebSocket upgrader with CORS support
    - Import gorilla/websocket and shared/retry packages

---
+++ services/api-gateway/ws.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"ride-sharing/shared/contracts"
+	"ride-sharing/shared/util"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleDriversWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println("Websocket upgrade failed: %v", err)
+	}
+
+	defer conn.Close()
+
+	userId := r.URL.Query().Get("userId")
+
+	if userId == "" {
+		log.Println("No user ID provided")
+		return
+	}
+
+	packageSlug := r.URL.Query().Get("packageSlug")
+
+	if packageSlug == "" {
+		log.Println("No packageSlug provided")
+		return
+	}
+
+	type Driver struct {
+		Id             string `json: "id"`
+		Name           string `json: "name"`
+		ProfilePicture string `json: "profile_picture"`
+		CarPlate       string `json: "car_plate"`
+		PackageSlug    string `json: "package_slug"`
+	}
+
+	msg := contracts.WSMessage{
+		Type: "driver.cmd.register",
+		Data: Driver{
+			Id:             userId,
+			Name:           "Tiago",
+			ProfilePicture: util.GetRandomAvatar(1),
+			CarPlate:       "ABC123",
+			PackageSlug:    packageSlug,
+		},
+	}
+
+	if err := conn.WriteJSON(msg); err != nil {
+		log.Printf("Error sending message: %v", err)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
+}
+
+func handleRidersWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Printf("Websocket upgrade failed: %v", err)
+	}
+
+	defer conn.Close()
+
+	userId := r.URL.Query().Get("userId")
+
+	if userId == "" {
+		log.Printf("No user ID provided")
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
+}
