7930889416fee760c0f1046b3b2075787668a458
Ujjwal <ujjwalsbx@gmail.com>
2025-10-07 19:46:14 +0530

    feat: add OSRMApiResponse types to trip-service pkg
    Added OSRMApiResponse struct and ToRoute() method to handle OSRM API responses in the trip-service package with protobuf conversion functionality

---
+++ services/trip-service/pkg/types/types.go
@@ -0,0 +1,34 @@
+package pkgtypes
+
+import pb "ride-sharing/shared/proto/trip"
+
+type OSRMApiResponse struct {
+	Route []struct {
+		Distance float64 `json:"distance"`
+		Duration float64 `json:"duration"`
+		Geometry struct {
+			Coordinates [][]float64 `json:"coordinates"`
+		} `json:"geometry"`
+	} `json:"routes"`
+}
+
+func (o *OSRMApiResponse) ToRoute() *pb.Route {
+	route := o.Route[0]
+	geometry := route.Geometry.Coordinates
+	coordinates := make([]*pb.Coordinate, len(geometry))
+	for i, coord := range geometry {
+		coordinates[i] = &pb.Coordinate{
+			Latitude:  coord[0],
+			Longitude: coord[1],
+		}
+	}
+	return &pb.Route{
+		Distance: route.Distance,
+		Duration: route.Duration,
+		Geometry: []*pb.Geometry{
+			{
+				Coordinates: coordinates,
+			},
+		},
+	}
+}
