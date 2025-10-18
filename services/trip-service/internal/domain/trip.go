9cc36381f78369f4160e61d0ab7c3992cc3880be
Ujjwal <ujjwalsbx@gmail.com>
2025-10-18 14:17:55 +0530

    refactor(trip): standardize context parameter naming and update GenerateTripFares signature
    - Renamed `ctx` to `context` for consistency in TripService interface methods
    - Updated `GenerateTripFares` to accept an additional OSRMApiResponse parameter, enabling enhanced fare generation logic

--- services/trip-service/internal/domain/trip.go
+++ services/trip-service/internal/domain/trip.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"context"
 	pkgtypes "ride-sharing/services/trip-service/pkg/types"
+	pb "ride-sharing/shared/proto/trip"
 	"ride-sharing/shared/types"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -13,18 +14,19 @@ type TripModel struct {
 	UserID   string
 	Status   string
 	RideFare *RideFareModel
+	Driver   *pb.TripDriver
 }
 
 type TripRepository interface {
-	CreateTrip(ctx context.Context, trip *TripModel) (*TripModel, error)
+	CreateTrip(context context.Context, trip *TripModel) (*TripModel, error)
 	SaveRideFare(context.Context, *RideFareModel) error
 	GetRideFareByID(context context.Context, id string) (*RideFareModel, error)
 }
 
 type TripService interface {
-	CreateTrip(ctx context.Context, fare *RideFareModel) (*TripModel, error)
-	GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*pkgtypes.OSRMApiResponse, error)
+	CreateTrip(context context.Context, fare *RideFareModel) (*TripModel, error)
+	GetRoute(context context.Context, pickup, destination *types.Coordinate) (*pkgtypes.OSRMApiResponse, error)
 	EstimatePackagePriceWithRoute(*pkgtypes.OSRMApiResponse) []*RideFareModel
-	GenerateTripFares(context.Context, []*RideFareModel, string) ([]*RideFareModel, error)
+	GenerateTripFares(context.Context, []*RideFareModel, string, *pkgtypes.OSRMApiResponse) ([]*RideFareModel, error)
 	GetAndValidateFare(context context.Context, fareID, userID string) (*RideFareModel, error)
 }
